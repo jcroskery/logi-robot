3857a2916a0e065b37b137d0e50144b6245d52fa
Justus Croskery <justus@olmmcc.tk>
2021-04-12 15:59:24 -0400

    Try uploading everything

---
+++ target/armv7-unknown-linux-gnueabihf/debug/deps/rpr-5f2de032b5467a68.d
@@ -0,0 +1,5 @@
+/home/justus/Rust/Rust/rpr/target/armv7-unknown-linux-gnueabihf/debug/deps/rpr-5f2de032b5467a68: src/main.rs
+
+/home/justus/Rust/Rust/rpr/target/armv7-unknown-linux-gnueabihf/debug/deps/rpr-5f2de032b5467a68.d: src/main.rs
+
+src/main.rs:
---
+++ target/armv7-unknown-linux-gnueabihf/debug/rpr.d
@@ -0,0 +1,1 @@
+/home/justus/Rust/Rust/rpr/target/armv7-unknown-linux-gnueabihf/debug/rpr: /home/justus/Rust/Rust/rpr/src/main.rs
