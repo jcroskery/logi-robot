68a1e6fa74599275fd437f4e75a6481002e572a8
Justus Croskery <justus@olmmcc.tk>
2021-04-13 17:14:05 -0400

    Default Commit Message

--- src/gyro.rs
+++ src/gyro.rs
@@ -1,14 +1,14 @@
 use rppal::i2c::I2c;
 
-const GYROADDRESS: u16 = 0x69;
+const GYROADDRESS: u16 = 0x68;
 const POWERREGISTER: u8 = 0x6b;
 const GYROREGISTER: u8 = 0x3b;
 
 pub fn gyro() {
     let mut i2c = I2c::new().unwrap();
     let mut buffer = [0; 6];
-    i2c.set_slave_address(GYROADDRESS);
-    i2c.block_write(POWERREGISTER, &[0x01]);
-    i2c.block_read(GYROREGISTER, &mut buffer);
+    i2c.set_slave_address(GYROADDRESS).unwrap();
+    i2c.block_write(POWERREGISTER, &[0x01]).unwrap();
+    i2c.block_read(GYROREGISTER, &mut buffer).unwrap();
     println!("{:?}", buffer);
 }
