bf637780076f9a3a82bb5ca81f31d79544aca3ed
Justus Croskery <justus@olmmcc.tk>
2021-04-13 15:35:11 -0400

    Default Commit Message

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,6 @@ fn main() {
     });
     */
     motor::drive(gpio.clone(), &pwm, &[100, 100]);
-    spin_sleep::sleep(Duration::from_millis(5000));
     println!("Finished sleep. Exiting.");
-    //motor::drive(gpio, &pwm, &[0, 0]);
+    motor::drive(gpio, &pwm, &[0, 0]);
 }
