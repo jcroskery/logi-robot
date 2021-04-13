bf0a8dfb784349b8a07cb6b5cb0b983ade158aed
Justus Croskery <justus@olmmcc.tk>
2021-04-13 14:48:21 -0400

    Default Commit Message

--- src/motor.rs
+++ src/motor.rs
@@ -8,9 +8,6 @@ const DIRECTIONPINS: &[u8] = &[19, 26, 20, 21];
 pub async fn drive(gpio: Gpio, pwm: &[Pwm], speeds: &[i32]) {
     let map = |pin_number: &u8| { gpio.get(*pin_number).unwrap().into_output()};
     let mut enable_pins: Vec<_> = DIRECTIONPINS.iter().map(map).collect();
-    for i in speed_pins.iter() {
-        println!("Speed pin: {}", i.is_set_high());
-    }
     for i in enable_pins.iter() {
         println!("Enable pin: {}", i.is_set_high());
     }
