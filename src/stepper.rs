dcec5e430ad31c3b794c9d5b79451b820ef99919
Justus Croskery <justus@olmmcc.tk>
2021-04-13 12:38:56 -0400

    Default Commit Message

--- src/stepper.rs
+++ src/stepper.rs
@@ -7,7 +7,7 @@ const STEPS: &[usize] = &[1, 0, 2, 1, 3, 2, 0, 3];
 pub async fn init_stepper_pins(gpio: Gpio) {
     loop {
         let mut pins = [gpio.get(PINS[0]).unwrap().into_output(), gpio.get(PINS[1]).unwrap().into_output(), gpio.get(PINS[2]).unwrap().into_output(), gpio.get(PINS[3]).unwrap().into_output()];
-        //println!("{}\n", stepper(&mut pins, 90).await);
+        println!("{}\n", stepper(&mut pins, 90).await);
         println!("{}\n", stepper(&mut pins, -90).await);
     }
 }
@@ -20,7 +20,7 @@ async fn stepper(pins: &mut [OutputPin], dist: i32) -> i32 {
     for i in 0..((dist.abs() as f32 / 360.0 * 512.0 / 64.0 * 63.68395) as i32) {
         let fore = |j: usize| {
             if pins[STEPS[j]].is_set_high() { pins[STEPS[j]].set_low(); } else { pins[STEPS[j]].set_high(); }
-            spin_sleep::sleep(Duration::from_micros(900000));
+            spin_sleep::sleep(Duration::from_micros(700));
         };
         if dist < 0 { 
             (0..8).rev().for_each(fore); 
