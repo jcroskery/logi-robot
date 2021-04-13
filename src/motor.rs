cb9ae3967ead1b579a4d5fb67408df9288c6f59b
Justus Croskery <justus@olmmcc.tk>
2021-04-13 16:26:48 -0400

    Default Commit Message

--- src/motor.rs
+++ src/motor.rs
@@ -10,7 +10,7 @@ pub fn drive(enable_pins: &mut Vec<OutputPin>, direction_pins: &mut Vec<OutputPi
     for i in 0..2 {
         enable_pins[i].set_pwm_frequency(100.0, speeds[i].abs() as f64 / 100.0).unwrap();
         //speed_pins[i].set_high();
-        if speeds[i] > 0 {
+        if speeds[i] < 0 {
             direction_pins[i * 2].set_high();
             direction_pins[i * 2 + 1].set_low();
         } else {
