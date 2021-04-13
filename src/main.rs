7b0ef684d309d5bd13d74ad9b3d8413548796115
Justus Croskery <justus@olmmcc.tk>
2021-04-13 15:36:40 -0400

    Default Commit Message

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn main() {
         stepper::init_stepper_pins(gpio).await;
     });
     */
-    motor::drive(gpio.clone(), &pwm, &[100, 100]);
+    motor::drive(gpio, &pwm, &[100, 100]);
     spin_sleep::sleep(Duration::from_millis(5000));
     println!("Finished sleep. Exiting.");
     motor::drive(gpio, &pwm, &[0, 0]);
