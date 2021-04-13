a2eec2fdf808a4b44fdb2bab7766a87722b54bf6
Justus Croskery <justus@olmmcc.tk>
2021-04-13 19:26:59 -0400

    Default Commit Message

--- src/gyro.rs
+++ src/gyro.rs
@@ -1,4 +1,5 @@
 use rppal::i2c::I2c;
+use std::time::Duration;
 
 const GYROADDRESS: u16 = 0x68;
 const POWERREGISTER: u8 = 0x6b;
@@ -9,6 +10,7 @@ pub fn gyro() {
     let mut buffer = [0; 14];
     i2c.set_slave_address(GYROADDRESS).unwrap();
     i2c.block_write(POWERREGISTER, &[0x01]).unwrap();
+    spin_sleep::sleep(Duration::from_millis(10));
     i2c.block_read(GYROREGISTER, &mut buffer).unwrap();
     for i in 0..7 {
         if i == 3 {continue;}
