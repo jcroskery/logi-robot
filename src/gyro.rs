80e687561ddf99dde5a6260c1a68b47ae074b8ca
Justus Croskery <justus@olmmcc.tk>
2021-04-13 17:15:11 -0400

    Default Commit Message

--- src/gyro.rs
+++ src/gyro.rs
@@ -6,7 +6,7 @@ const GYROREGISTER: u8 = 0x3b;
 
 pub fn gyro() {
     let mut i2c = I2c::new().unwrap();
-    let mut buffer = [0; 6];
+    let mut buffer = [0; 14];
     i2c.set_slave_address(GYROADDRESS).unwrap();
     i2c.block_write(POWERREGISTER, &[0x01]).unwrap();
     i2c.block_read(GYROREGISTER, &mut buffer).unwrap();
