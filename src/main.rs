0cd8055f0c1c80f20e2b0ed35ed8098ee24c8f6d
Justus Croskery <justus@olmmcc.tk>
2021-04-13 08:58:50 -0400

    Default Commit Message

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ async fn main() {
     let gpio = Gpio::new().unwrap();
 
     let handle = tokio::spawn(async {
+        println!("Hello, us!");
         ultrasonic::init_ultrasonic_pins(gpio).await;
     });
 
-    println!("Hello, world!");
+    println!("Hello, you!");
     handle.await.unwrap();
 }
