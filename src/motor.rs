a486c37019baaf752139d21c770571b13699d344
Justus Croskery <justus@olmmcc.tk>
2021-04-13 15:46:46 -0400

    Default Commit Message

--- src/motor.rs
+++ src/motor.rs
@@ -3,14 +3,16 @@ use tokio::time::Duration;
 use rppal::pwm::{Pwm, Channel, Polarity};
 
 use std::convert::TryInto;
-const ENABLEPINS: &[u8] = &[18, 19];
-const DIRECTIONPINS: &[u8] = &[13, 26, 20, 21];
 
-pub fn drive(gpio: Gpio, speeds: &[i32]) {
+const DIRECTIONPINS: &[u8] = &[13, 26, 20, 21];
+const ENABLEPINS: &[u8] = &[18, 19];
+
+pub fn drive(gpio: Gpio, enablespeeds: &[i32]) {
     let map = |pin_number: &u8| { gpio.get(*pin_number).unwrap().into_output()};
     let mut direction_pins: Vec<_> = DIRECTIONPINS.iter().map(map).collect();
     let mut enable_pins: Vec<_> = ENABLEPINS.iter().map(map).collect();
     for i in 0..2 {
+        enable_pins[i].set_reset_on_drop(false);
         enable_pins[i].set_pwm_frequency(100.0, speeds[i].abs() as f64 / 100.0).unwrap();
         //speed_pins[i].set_high();
         if speeds[i] > 0 {
