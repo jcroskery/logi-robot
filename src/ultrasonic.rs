16e0560fac0fd57d79437452097a508786d610f6
Justus Croskery <justus@olmmcc.tk>
2021-04-13 11:19:14 -0400

    Default Commit Message

--- src/ultrasonic.rs
+++ src/ultrasonic.rs
@@ -1,6 +1,8 @@
 use tokio::time::{self, Duration};
 use rppal::gpio::{OutputPin, InputPin, Gpio, Trigger, Level};
 
+use std::sync::mpsc::channel;
+
 //use std::sync::{Arc, Mutex};
 
 const TRIGGERPIN: u8 = 14;
@@ -8,7 +10,6 @@ const ECHOPIN: u8 = 15;
 
 pub async fn init_ultrasonic_pins(gpio: Gpio) {
     loop {
-        println!("This happens, good");
         ultrasonic(gpio.get(TRIGGERPIN).unwrap().into_output(), gpio.get(ECHOPIN).unwrap().into_input()).await;
     }
 }
@@ -21,13 +22,15 @@ async fn ultrasonic (mut trigger_pin: OutputPin, mut echo_pin: InputPin) {
     trigger_pin.set_low();
     let mut timer = howlong::HighResolutionTimer::new();
     timer.stop();
+    let (sender, receiver) = channel();
     echo_pin.set_async_interrupt(Trigger::Both, move |level| {
         if level == Level::High {
-            println!("Happens");
             timer.start();
         } else {
             println!("{}\n", timer.elapsed().subsec_nanos() as f32 / 1000.0 * 0.017);
+            sender.send(true).unwrap();
         }
     }).unwrap();
     interval.tick().await;
+    receiver.recv().unwrap();
 }
