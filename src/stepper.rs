1d3772e0071ed91ee08eca7bc20793917145771c
Justus Croskery <justus@olmmcc.tk>
2021-04-13 12:24:14 -0400

    Default Commit Message

--- src/stepper.rs
+++ src/stepper.rs
@@ -7,22 +7,23 @@ const STEPS: &[usize] = &[1, 0, 2, 1, 3, 2, 0, 3];
 pub async fn init_stepper_pins(gpio: Gpio) {
     loop {
         let mut pins = [gpio.get(PINS[0]).unwrap().into_output(), gpio.get(PINS[1]).unwrap().into_output(), gpio.get(PINS[2]).unwrap().into_output(), gpio.get(PINS[3]).unwrap().into_output()];
-        println!("{}\n", stepper(&mut pins).await);
+        println!("{}\n", stepper(&mut pins, 90).await);
+        println!("{}\n", stepper(&mut pins, -90).await);
         break;
     }
 }
 
-async fn stepper(pins: &mut [OutputPin]) -> i32 {
+async fn stepper(pins: &mut [OutputPin], dist: i32) -> i32 {
     pins[0].set_high();
     for i in 1..4 {
         pins[i].set_low();
     }
-    for i in 0..((90.0 / 360.0 * 512.0 / 64.0 * 63.68395) as i32) {
-        for j in 0..8 {
+    for i in 0..((abs(dist) as f32 / 360.0 * 512.0 / 64.0 * 63.68395) as i32) {
+        (if dist < 0 { 8..0 } else { 0..8 }).for_each(|j| {
             if pins[STEPS[j]].is_set_high() { pins[STEPS[j]].set_low(); } else { pins[STEPS[j]].set_high(); }
             spin_sleep::sleep(Duration::from_micros(900));
-        }
+        });
     }
     pins[0].set_low();
-    return 90;
+    return dist;
 }
