c38b9f50d369be1f3d38fd83829ab7ae7e344c4b
Justus Croskery <justus@olmmcc.tk>
2021-04-13 12:37:13 -0400

    Default Commit Message

--- src/stepper.rs
+++ src/stepper.rs
@@ -7,7 +7,7 @@ const STEPS: &[usize] = &[1, 0, 2, 1, 3, 2, 0, 3];
 pub async fn init_stepper_pins(gpio: Gpio) {
     loop {
         let mut pins = [gpio.get(PINS[0]).unwrap().into_output(), gpio.get(PINS[1]).unwrap().into_output(), gpio.get(PINS[2]).unwrap().into_output(), gpio.get(PINS[3]).unwrap().into_output()];
-        println!("{}\n", stepper(&mut pins, 90).await);
+        //println!("{}\n", stepper(&mut pins, 90).await);
         println!("{}\n", stepper(&mut pins, -90).await);
     }
 }
