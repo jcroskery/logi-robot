8d68f93a05ca1d300d6c808a116a54d8d1c52e44
Justus Croskery <justus@olmmcc.tk>
2021-04-13 13:23:24 -0400

    Default Commit Message

--- src/motor.rs
+++ src/motor.rs
@@ -11,8 +11,8 @@ pub async fn drive(gpio: Gpio, speeds: &[i32]) {
     let mut speed_pins: Vec<_> = SPEEDPINS.iter().map(map).collect();
     let mut enable_pins: Vec<_> = DIRECTIONPINS.iter().map(map).collect();
     for i in 0..2 {
-        speed_pins[i].set_pwm(Duration::from_millis(10), 
-        Duration::from_micros((50 * speeds[i].abs()).try_into().unwrap()));
+        //speed_pins[i].set_pwm(Duration::from_millis(10), Duration::from_micros((50 * speeds[i].abs()).try_into().unwrap()));
+        speed_pins[i].set_high();
         if speeds[i] < 0 {
             enable_pins[i * 2].set_high();
             enable_pins[i * 2 + 1].set_low();
