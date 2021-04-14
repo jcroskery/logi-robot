5a7c5044d7e60d5acd0cbfa4e84015ed5ff80424
Justus Croskery <justus@olmmcc.tk>
2021-04-13 21:27:16 -0400

    Default Commit Message

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rppal::gpio::Gpio;
+use rppal::gpio::{Gpio, Mode};
 use rppal::pwm::{Pwm, Channel, Polarity};
 use std::time::Duration;
 
@@ -7,8 +7,13 @@ mod infrared;
 mod stepper;
 mod motor;
 mod gyro;
+mod servos;
 
 const DIRECTIONPINS: &[u8] = &[20, 21, 13, 26];
+
+const LEDPIN: u8 = 9;
+const ULTRASONICPIN: u8 = 10;
+const INFRAREDPIN: u8 = 11;
 
 fn main() {
     let gpio = Gpio::new().unwrap();
@@ -17,7 +22,11 @@ fn main() {
             Polarity::Normal, true).unwrap(), 
             Pwm::with_frequency(Channel::Pwm1,100.0, 0.0,
             Polarity::Normal, true).unwrap()];
-            
+    //let mut servo_pins = [gpio.get(LEDPIN).unwrap().into_io(Mode::Output), 
+    //    gpio.get(ULTRASONICPIN).unwrap().into_io(Mode::Output), 
+    //    gpio.get(INFRAREDPIN).unwrap().into_io(Mode::Output)];
+    servos::send_bytes(gpio.clone(), LEDPIN, &[254, 0, 0, 0], 0);
+    println!("{}", servos::receive_byte(gpio.clone(), LEDPIN));
     /*
     let ultrasonic_gpio = gpio.clone();
     tokio::spawn(async {
@@ -36,8 +45,8 @@ fn main() {
     spin_sleep::sleep(Duration::from_millis(5000));
     println!("Finished sleep. Exiting.");
     motor::drive(&mut pwm, &mut direction_pins, &[0, 0]);
-    */
     loop {
         gyro::gyro();
     }
+    */
 }
---
+++ src/servos.rs
@@ -0,0 +1,71 @@
+use rppal::gpio::{OutputPin, InputPin, Gpio, Trigger, Level, Mode};
+use std::time::Duration;
+
+use std::sync::mpsc::channel;
+
+const BITDELAY: u64 = 417;
+
+fn send_byte(pin: &mut OutputPin, byte: u8) {
+    let sleeper = spin_sleep::SpinSleeper::default();
+    pin.set_low();
+    sleeper.sleep(Duration::from_micros(BITDELAY));
+    for i in 0..8 {
+        if (byte >> i) & 0x01 == 1 {
+            pin.set_high();
+        } else {
+            pin.set_low();
+        }
+        sleeper.sleep(Duration::from_micros(BITDELAY));
+    }
+    pin.set_high();
+    sleeper.sleep(Duration::from_micros(BITDELAY * 2));
+}
+
+fn calculate_checksum(bytes: &[u8], module: u8) -> u8 {
+    let mut sum: u16 = bytes.iter().map(|u8byte| { *u8byte as u16 }).sum();
+    sum += sum >> 8;
+    sum += sum << 4;
+    sum &= 0xf0;
+    return (sum as u8) + module;
+}
+
+pub fn send_bytes(gpio: Gpio, pin_number: u8, bytes: &[u8], module: u8) {
+    let mut pin = gpio.get(pin_number).unwrap().into_output();
+    send_byte(&mut pin, 0xff);
+    for i in 0..4 {
+        send_byte(&mut pin, bytes[i]);
+    }
+    send_byte(&mut pin, calculate_checksum(bytes, module));
+}
+
+pub fn receive_byte(gpio: Gpio, pin_number: u8) -> u8 {
+    let mut pin = gpio.get(pin_number).unwrap().into_input();
+    let mut received_byte = 0;
+    let mut timer = howlong::HighResolutionTimer::new();
+    timer.stop();
+    let (sender, receiver) = channel();
+    pin.set_async_interrupt(Trigger::Both, move |level| {
+        if level == Level::High {
+            sender.send(true).unwrap();
+        } else {
+            sender.send(false).unwrap();
+        }
+    }).unwrap();
+    for i in 0..8 {
+        if receiver.recv().unwrap() {
+            timer.start();
+        } else {
+            return 0;
+        }
+        if !receiver.recv().unwrap() {
+            if(timer.elapsed().as_micros() > 400) {
+                received_byte |= 1 << i;
+            }
+            timer.stop();
+        } else {
+            return 0;
+        }
+    }
+    return received_byte;
+}
+
