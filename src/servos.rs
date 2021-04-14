f6b4f7989acebbafd4ef1636be7a182f43b44c1c
Justus Croskery <justus@olmmcc.tk>
2021-04-14 14:50:00 -0400

    Default Commit Message

--- src/servos.rs
+++ src/servos.rs
@@ -148,14 +148,14 @@ struct Led {
 impl Led {
     fn update_colour_bit_1(&mut self) -> bool {
         let mut bytes = self.get_bytes();
-        bytes[self.get_module_position() as usize] = (self.colour.2 >> 3) + self.colour.1;
+        bytes[self.get_module_position() as usize] = (self.colour.1 >> 3) + self.colour.0;
         self.set_bytes(bytes.clone());
         self.try_send_and_receive(Some(0x02), None)
     }
 
     fn update_colour_bit_2(&mut self) -> bool {
         let mut bytes = self.get_bytes();
-        bytes[self.get_module_position() as usize] = 0x40 | self.colour.0;
+        bytes[self.get_module_position() as usize] = 0x40 | self.colour.2;
         self.set_bytes(bytes.clone());
         self.try_send_and_receive(Some(0x02), None)
     }
