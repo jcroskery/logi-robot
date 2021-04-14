1d3b424214c5c12b6fa92d18e523a9aa062f94a2
Justus Croskery <justus@olmmcc.tk>
2021-04-14 12:04:08 -0400

    Default Commit Message

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     //    gpio.get(INFRAREDPIN).unwrap().into_io(Mode::Output)];
     //servos::send_bytes(gpio.clone(), LEDPIN, &[254, 0, 0, 0], 0);
     //println!("{}", servos::receive_byte(gpio.clone(), LEDPIN));
-    let infrared_chain = servos::ServoChain::new(gpio, INFRAREDPIN, 
+    let infrared_chain = servos::ServoChain::new(gpio, ULTRASONICPIN, 
         vec![servos::ServoType::MOTOR, servos::ServoType::MOTOR]);
     
     //let ultrasonic_gpio = gpio.clone();
