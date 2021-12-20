2559a3be6a17f424021c33cf9859e84ee3129071
jcroskery <justus@olmmcc.tk>
2021-12-20 18:34:45 -0500

    Log responses

--- exporttopi.sh
+++ exporttopi.sh
@@ -1,4 +1,4 @@
 #Every time
 git push
-ssh -t pi@169.254.163.150 "cd rpr; git pull; cargo run"
+ssh -t pi@169.254.163.150 "cd rpr; git pull; /home/pi/.cargo/bin/cargo run"
 # systemctl --user stop logi; systemctl --user start logi
