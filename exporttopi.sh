9869d0c4e8242bfdf1ae8b4b2d4b04b4640a0250
jcroskery <justus@olmmcc.tk>
2021-12-19 21:02:56 -0500

    Add more docker stuff

--- exporttopi.sh
+++ exporttopi.sh
@@ -1,5 +1,6 @@
 #Every time
 cross build --release --target=armv7-unknown-linux-gnueabihf
+git 
 ssh -t pi@169.254.163.150 'systemctl --user stop logi'
 scp /home/justus/Rust/Rust/rpr/target/armv7-unknown-linux-gnueabihf/release/rpr pi@169.254.163.150:/home/pi/logi/rpr
 ssh -t pi@169.254.163.150 'systemctl --user start logi'
