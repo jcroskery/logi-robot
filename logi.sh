a0071c110049c86a0463cdc34e187dd11ce3e2a9
jcroskery <justus@olmmcc.tk>
2021-12-23 11:47:11 -0500

    Add shell script to initialize webcam

---
+++ logi.sh
@@ -0,0 +1,3 @@
+v4l2-ctl -d /dev/video2 --set-ctrl=focus_auto=0
+v4l2-ctl -d /dev/video2 --set-ctrl=focus_absolute=0
+target/debug/rpr
--- opencvtopi.sh
+++
@@ -1,2 +0,0 @@
-#One time only
-scp -r /home/justus/Rust/Rust/rpr/docker/opencv pi@169.254.163.150:/opt/opencv/
